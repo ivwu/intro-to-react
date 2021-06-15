355342683e8f6b4ae2d37b857f179d290c071293
ivwu22 <ivywu6@gmail.com>
2021-06-15 14:04:39 -0400

    shows message if no message

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -50,77 +50,91 @@ class ContactForm extends Component {
     });
   };
 
+  handleReset = () => {
+    this.setState({
+      submitted: false,
+      firstName: "",
+      lastName: "",
+      email: "",
+      message: "",
+    });
+  };
+
   render() {
     const { firstName, lastName, email, message, submitted } = this.state;
+    if (!submitted) {
+      return (
+        <form className="Form-container" onSubmit={this.handleSubmit}>
+          {" "}
+          <label htmlFor="firstName" className="Form-label">
+            First Name:{" "}
+            <input
+              id="firstName"
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="lastName">
+            Last Name:{" "}
+            <input
+              id="lastName"
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="email">
+            Email:{" "}
+            <input
+              id="email"
+              type="text"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="message">
+            Message:{" "}
+            <input
+              id="message"
+              type="text"
+              name="message"
+              value={message}
+              onChange={this.handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <input type="submit" value="Submit" />{" "}
+        </form>
+      );
+    }
+    if (message === "") {
+      return (
+        <>
+          <h1>Hey {firstName}, the message field cannot be empty</h1>
+          <button onClick={this.handleReset}>Reset Form</button>
+        </>
+      );
+    }
     return (
       <div>
-        {/* <h2>{firstName}</h2>
-        <h2>{lastName}</h2>
-        <h2>{email}</h2>
-        <h2>{message}</h2> */}
-        <form className="Form-container" onSubmit={this.handleSubmit}>
-          {!submitted ? (
-            <>
-              {" "}
-              <label htmlFor="firstName" className="Form-label">
-                First Name:
-                <input
-                  id="firstName"
-                  type="text"
-                  name="firstName"
-                  value={firstName}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <br />
-              <br />
-              <label htmlFor="lastName">
-                Last Name:
-                <input
-                  id="lastName"
-                  type="text"
-                  name="lastName"
-                  value={lastName}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <br />
-              <br />
-              <label htmlFor="email">
-                Email:
-                <input
-                  id="email"
-                  type="text"
-                  name="email"
-                  value={email}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <br />
-              <br />
-              <label htmlFor="message">
-                Message:
-                <input
-                  id="message"
-                  type="text"
-                  name="message"
-                  value={message}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <br />
-              <br />
-              <input type="submit" value="Submit" />{" "}
-            </>
-          ) : (
-            <section>
-              <p>
-                Thank you, {firstName}, for submitting the form with the
-                following message: {message}
-              </p>
-            </section>
-          )}
-        </form>
+        <section>
+          <p>
+            Thank you, {firstName}, for submitting the form with the following
+            message: {message}
+          </p>
+          <button onClick={this.handleReset}>Reset Form</button>
+        </section>
       </div>
     );
   }
