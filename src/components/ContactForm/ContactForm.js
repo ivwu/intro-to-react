c40db074a9df28eb195ef031cd81314922d3c98e
ivwu22 <ivywu6@gmail.com>
2021-06-15 11:25:55 -0400

    adds controlled inputs

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,61 +1,101 @@
 import React, { Component } from "react";
+import "./ContactForm.css";
 
 class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "Ivy",
-      lastName: "Wu",
-      email: "ivy@test.com",
-      message: "I am sleepy",
+      firstName: "",
+      lastName: "",
+      email: "",
+      message: "",
     };
   }
+
+  handleFirstNameChange = (event) => {
+    this.setState({ firstName: event.target.value }); //grab the value from the first name input field
+  };
+
+  handleLastNameChange = (event) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
+
+  handleEmailChange = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
+  handleMessageChange = (event) => {
+    this.setState({
+      message: event.target.value,
+    });
+  };
+
   render() {
     const { firstName, lastName, email, message } = this.state;
     return (
       <div>
-        <h1>
-          Welcome to {firstName} {lastName}'s Page
-        </h1>
-        <h2>Contact at {email}</h2>
-        <h3>Current mood: {message}</h3>
+        <h2>{firstName}</h2>
+        <h2>{lastName}</h2>
+        <h2>{email}</h2>
+        <h2>{message}</h2>
+        <form className="Form-container">
+          <label htmlFor="firstName" className="Form-label">
+            First Name:
+            <input
+              id="firstName"
+              type="text"
+              name="name"
+              value={firstName}
+              onChange={this.handleFirstNameChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="lastName">
+            Last Name:
+            <input
+              id="lastName"
+              type="text"
+              name="name"
+              value={lastName}
+              onChange={this.handleLastNameChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              type="text"
+              name="name"
+              value={email}
+              onChange={this.handleEmailChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="message">
+            Message:
+            <input
+              id="message"
+              type="text"
+              name="name"
+              value={message}
+              onChange={this.handleMessageChange}
+            />
+          </label>
+          <br />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
 }
 
 export default ContactForm;
-
-// 1. props are static while state can be changed (setState)
-// 2. you can define the state with a constructor and super within class component
-// 3. state can be updated with setState
-// 4. 'create-react-app' is a framework that helps make the backbone of a react app and let's programmer start their react coding quickly. code for components will be added in the "src" folder with a components folder made within it to store all components. The `index.js` file is the entry point of the application and holds the ReactDOM rendering of the App.js. App.js is the top-level component that stores all the other components to be imported into index.js
-// 5. The import/export statements are for components and it's use to allow components to be moved within and out of their own files
-// 6.
-// import React, { Component } from "react";
-
-// class ContactForm extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       firstName: "Ivy",
-//       lastName: "Wu",
-//       email: "ivy@test.com",
-//       message: "I am sleepy",
-//     };
-//   }
-//   render() {
-//     const { firstName, lastName, email, message } = this.state;
-//     return (
-//       <div>
-//         <h1>
-//           Welcome to {firstName} {lastName}'s Page
-//         </h1>
-//         <h2>Contact at {email}</h2>
-//         <h3>Current mood: {message}</h3>
-//       </div>
-//     );
-//   }
-// }
-
-// export default ContactForm;
