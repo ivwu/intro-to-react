3ab4aa2dec0fb188e7425d860c3f53ce04cc6f28
ivwu22 <ivywu6@gmail.com>
2021-06-15 12:28:07 -0400

    changes what returns/renders depending on if form is submitted

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,6 +5,7 @@ class ContactForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      submitted: false,
       firstName: "",
       lastName: "",
       email: "",
@@ -42,64 +43,83 @@ class ContactForm extends Component {
     });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      submitted: true,
+    });
+  };
+
   render() {
-    const { firstName, lastName, email, message } = this.state;
+    const { firstName, lastName, email, message, submitted } = this.state;
     return (
       <div>
-        <h2>{firstName}</h2>
+        {/* <h2>{firstName}</h2>
         <h2>{lastName}</h2>
         <h2>{email}</h2>
-        <h2>{message}</h2>
-        <form className="Form-container">
-          <label htmlFor="firstName" className="Form-label">
-            First Name:
-            <input
-              id="firstName"
-              type="text"
-              name="firstName"
-              value={firstName}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <br />
-          <label htmlFor="lastName">
-            Last Name:
-            <input
-              id="lastName"
-              type="text"
-              name="lastName"
-              value={lastName}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <br />
-          <label htmlFor="email">
-            Email:
-            <input
-              id="email"
-              type="text"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <br />
-          <label htmlFor="message">
-            Message:
-            <input
-              id="message"
-              type="text"
-              name="message"
-              value={message}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <br />
-          <input type="submit" value="Submit" />
+        <h2>{message}</h2> */}
+        <form className="Form-container" onSubmit={this.handleSubmit}>
+          {!submitted ? (
+            <>
+              {" "}
+              <label htmlFor="firstName" className="Form-label">
+                First Name:
+                <input
+                  id="firstName"
+                  type="text"
+                  name="firstName"
+                  value={firstName}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <br />
+              <label htmlFor="lastName">
+                Last Name:
+                <input
+                  id="lastName"
+                  type="text"
+                  name="lastName"
+                  value={lastName}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <br />
+              <label htmlFor="email">
+                Email:
+                <input
+                  id="email"
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <br />
+              <label htmlFor="message">
+                Message:
+                <input
+                  id="message"
+                  type="text"
+                  name="message"
+                  value={message}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <br />
+              <br />
+              <input type="submit" value="Submit" />{" "}
+            </>
+          ) : (
+            <section>
+              <p>
+                Thank you, {firstName}, for submitting the form with the
+                following message: {message}
+              </p>
+            </section>
+          )}
         </form>
       </div>
     );
